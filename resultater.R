6d5bc1316c3a7c3129ec136cdfb61c4e1537a1f5
Håkon Malmedal <hmalmedal@gmail.com>
2012-02-06 21:17:33 +0100

    Skriver liste over resultatene til fil med R.

---
+++ resultater.R
@@ -0,0 +1,29 @@
+source("partiplott.R")
+sidelinje <- function(i) {
+  return(paste("\\res{", i, "}", sep = ""))
+}
+kapittellinje <- function(fylke) {
+  return(paste("\\section{", fylke, "}", sep = ""))
+}
+resultatlinjer <- c(kapittellinje("Hele landet"),
+                    sapply(1:length(Partiliste$Partinavn), sidelinje))
+fylkespartiantall <- function(fylke) {
+  return(length(fylkesparti(fylke)))
+}
+fylkesstartnummer <- cumsum(c(length(Partiliste$Partinavn),
+                              sapply(Fylkesliste$Fylkesnavn[-3],
+                                     fylkespartiantall))) + 1
+length(fylkesstartnummer) <- length(fylkesstartnummer) - 1
+fylkeslinjer <- function(fylke) {
+  startnummer <- fylkesstartnummer[match(fylke, Fylkesliste$Fylkesnavn[-3])]
+  return(c(kapittellinje(fylke),
+           sapply(seq.int(from = startnummer,
+                          length.out = fylkespartiantall(fylke)),
+                  sidelinje)))
+}
+for (fylke in Fylkesliste$Fylkesnavn[-3]) {
+  resultatlinjer <- append(resultatlinjer, fylkeslinjer(fylke))
+}
+fileConn<-file("resultater.tex")
+writeLines(resultatlinjer, fileConn)
+close(fileConn)
