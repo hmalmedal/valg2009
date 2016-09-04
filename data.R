9991df3bb1fdf624e97dadc3db6e70edb83f791b
Håkon Malmedal <hmalmedal@gmail.com>
2016-09-04 11:03:07 +0200

    Bruker forcats

--- data.R
+++ data.R
@@ -1,12 +1,13 @@
 library(dplyr)
+library(forcats)
 library(readr)
 partier <- read_csv("partier.csv", col_types = "cc")
 kommuneresultater <- read_csv("Valgresultat_2009_kommuner.csv",
                               col_names = c("Fylke", "Kommune", "Parti",
                                             "Stemmer"),
                               col_types = "_cc_c_i_____", skip = 1) %>%
-  mutate(Fylke = factor(Fylke, unique(Fylke)),
-         Kommune = factor(Kommune, unique(Kommune)),
+  mutate(Fylke = fct_inorder(Fylke),
+         Kommune = fct_inorder(Kommune),
          Parti = factor(Parti, partier$Partiforkortelse, partier$Parti)) %>%
   group_by(Kommune) %>%
   mutate(Prosent = Stemmer / sum(Stemmer) * 100) %>%
