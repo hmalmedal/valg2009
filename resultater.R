71356769c62a7bab03dab3df8f0f4e08bd762619
Håkon Malmedal <hmalmedal@gmail.com>
2016-10-16 10:08:31 +0200

    Forenkling

--- resultater.R
+++ resultater.R
@@ -10,19 +10,18 @@ p <- kommuneresultater %>%
   mutate(Fylke = fct_inorder(Fylke)) %>%
   group_by(Fylke) %>%
   mutate(Fylkesstemmer = sum(Stemmer)) %>%
-  do(group_by(., Parti) %>%
-       arrange(Prosent) %>%
-       mutate(x = seq_along(Prosent)) %>%
-       mutate(Fylkesprosent = sum(Stemmer) / Fylkesstemmer * 100) %>%
-       do(p = ggplot(., aes(x = x, y = Prosent)) +
-            geom_point() +
-            geom_abline(slope = 0, intercept = .$Fylkesprosent[1]) +
-            geom_text(aes(label = Kommune, hjust = 1.1, vjust = 0.2),
-                      top_n(., 1, Prosent)) +
-            labs(title = str_c(.$Parti[1], .$Fylke[1], sep = "\n"),
-                 x = NULL, y = "Prosent") +
-            expand_limits(x = 0, y = 0)
-       )
+  group_by(Fylke, Parti) %>%
+  arrange(Prosent) %>%
+  mutate(x = seq_along(Prosent)) %>%
+  mutate(Fylkesprosent = sum(Stemmer) / Fylkesstemmer * 100) %>%
+  do(p = ggplot(., aes(x = x, y = Prosent)) +
+       geom_point() +
+       geom_abline(slope = 0, intercept = .$Fylkesprosent[1]) +
+       geom_text(aes(label = Kommune, hjust = 1.1, vjust = 0.2),
+                 top_n(., 1, Prosent)) +
+       labs(title = str_c(.$Parti[1], .$Fylke[1], sep = "\n"),
+            x = NULL, y = "Prosent") +
+       expand_limits(x = 0, y = 0)
   ) %>%
   ungroup()
 
