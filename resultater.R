d41cf2d28e05c5d0ec851079e67ac739d67af78d
Håkon Malmedal <hmalmedal@gmail.com>
2019-02-18 20:44:31 +0100

    Bruker group_split og group_keys

--- resultater.R
+++ resultater.R
@@ -1,9 +1,8 @@
 library(tidyverse)
-library(magrittr)
 
 source("data.R")
 
-p <- kommuneresultater %>%
+d <- kommuneresultater %>%
   bind_rows(mutate(., Fylke = "Hele landet"), .) %>%
   filter(Fylke != "Oslo") %>%
   mutate(Fylke = as_factor(Fylke)) %>%
@@ -12,25 +11,29 @@ p <- kommuneresultater %>%
   group_by(Fylke, Parti) %>%
   arrange(Prosent) %>%
   mutate(x = seq_along(Prosent)) %>%
-  mutate(Fylkesprosent = sum(Stemmer) / Total * 100) %>%
-  do(p = ggplot(., aes(x = x, y = Prosent)) +
-       geom_point() +
-       geom_abline(slope = 0, intercept = .$Fylkesprosent[1]) +
-       geom_text(aes(label = Kommune, hjust = 1.1, vjust = 0.2),
-                 top_n(., 1, Prosent)) +
-       labs(title = .$Parti[1], subtitle = .$Fylke[1],
-            x = NULL, y = "Prosent") +
-       expand_limits(x = 0, y = 0)
-  ) %>%
-  ungroup()
+  mutate(Fylkesprosent = sum(Stemmer) / Total * 100)
 
-p %>%
+p <- d %>%
+  group_split() %>%
+  map(~ggplot(., aes(x = x, y = Prosent)) +
+        geom_point() +
+        geom_abline(slope = 0, intercept = .$Fylkesprosent[1]) +
+        geom_text(aes(label = Kommune, hjust = 1.1, vjust = 0.2),
+                  top_n(., 1, Prosent)) +
+        labs(title = .$Parti[1], subtitle = .$Fylke[1],
+             x = NULL, y = "Prosent") +
+        expand_limits(x = 0, y = 0)
+  )
+
+d %>%
+  group_keys() %>%
   mutate(i = seq_along(Fylke)) %>%
   group_by(Fylke) %>%
   summarise(s = str_c("\\section{", first(Fylke), "}\n",
-                      str_c("\\res{", i, "}", collapse = "\n"))) %$%
-  cat(s, file = "resultater.tex", sep = "\n")
+                      str_c("\\res{", i, "}", collapse = "\n"))) %>%
+  pluck("s") %>%
+  write_lines("resultater.tex")
 
 pdf("resultater.pdf", height = 0.87 * 7)
-walk(p$p, print)
+walk(p, print)
 dev.off()
